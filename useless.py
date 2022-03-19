43d05516e7785f6194280e9eea33c5c029f5e2c5
MichielVerschueren99 <verschueren.m@hotmail.com>
2022-03-19 16:07:21 +0100

    bezig met exr

--- useless.py
+++ useless.py
@@ -1,56 +1,50 @@
+import imageio
 import numpy as np
 import OpenEXR as exr
 import Imath
+import torch
 
+import util
 
 def readEXR(filename):
-    """Read color + depth data from EXR image file.
 
-    Parameters
-    ----------
-    filename : str
-        File path.
-
-    Returns
-    -------
-    img : RGB or RGBA image in float32 format. Each color channel
-          lies within the interval [0, 1].
-          Color conversion from linear RGB to standard RGB is performed
-          internally. See https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_(CIE_XYZ_to_sRGB)
-          for more information.
-
-    Z : Depth buffer in float32 format or None if the EXR file has no Z channel.
-    """
-
-    exrfile = exr.InputFile(filename)
-    header = exrfile.header()
+    image_file = exr.InputFile(filename)
+    header = image_file.header()
 
     dw = header['dataWindow']
-    isize = (dw.max.y - dw.min.y + 1, dw.max.x - dw.min.x + 1)
+    i_size = (dw.max.y - dw.min.y + 1, dw.max.x - dw.min.x + 1)
 
     channelData = dict()
 
     # convert all channels in the image to numpy arrays
     for c in header['channels']:
-        C = exrfile.channel(c, Imath.PixelType(Imath.PixelType.FLOAT))
-        C = np.fromstring(C, dtype=np.float32)
-        C = np.reshape(C, isize)
+        C = image_file.channel(c, Imath.PixelType(Imath.PixelType.FLOAT))
+        C = np.frombuffer(C, dtype=np.float32)
+        C = np.reshape(C, i_size)
 
         channelData[c] = C
 
     colorChannels = ['R', 'G', 'B', 'A'] if 'A' in header['channels'] else ['R', 'G', 'B']
     img = np.concatenate([channelData[c][..., np.newaxis] for c in colorChannels], axis=2)
 
-    # linear to standard RGB
-    img[..., :3] = np.where(img[..., :3] <= 0.0031308,
-                            12.92 * img[..., :3],
-                            1.055 * np.power(img[..., :3], 1 / 2.4) - 0.055)
-
     # sanitize image to be in range [0, 1]
     img = np.where(img < 0.0, 0.0, np.where(img > 1.0, 1, img))
 
-    Z = None if 'Z' not in header['channels'] else channelData['Z']
+    assert 'Z' not in header['channels']
 
-    return img, Z
+    img = np.transpose(img, (2, 0, 1))
+    img = torch.from_numpy(img)
+    # min = img.min() + 0.0  # TODO ineens was hier + 0.0 nodig anders doet add_ raar om een of andere reden
+    # max = img.max() + 0.0
+    # img = torch.FloatTensor(img.size()).copy_(img)
+    # img.add_(-min).mul_(1.0 / (max - min))
+    # img = img.mul_(2).add_(-1)
 
-readEXR("C:\Users\Michi\Documents\school\Thesis-stuff\train_gt_1070.exr")
+    return img
+
+
+test = readEXR("D:\\Thesis\\train_gt_1070_indirect.exr")
+image1 = imageio.imread("D:\\Thesis\\train_gt_1070_indirect.png")
+image2 = util.load_image("D:\\Thesis\\train_gt_1070_indirect.pfm", "pfm")
+image3 = util.load_image("D:\\Thesis\\train_gt_1070_indirect.png", "png")
+print("test")
