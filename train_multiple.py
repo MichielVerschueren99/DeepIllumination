97683bec3926c455d75adfee99f21bf7b1f41dc8
Michiel Verschueren <r0705259@kuleuven.be>
2022-04-06 20:32:22 +0200

    experiment setup

--- train_multiple.py
+++ train_multiple.py
@@ -4,4 +4,11 @@ import train
 
 
 if __name__ == "__main__":
-    os.system("python train.py --dataset nm_test --n_epoch 3")
+
+    for _ in range(0, 1):
+        os.system("python train.py --dataset primitive_room_f1 --n_epoch 25 --gt_name indirect")
+
+    for _ in range(0, 1):
+        os.system("python train.py --dataset primitive_room_f1 --n_epoch 25 --gt_name indirect")
+
+    print("ALL TRAINING RUNS COMPLETED")
